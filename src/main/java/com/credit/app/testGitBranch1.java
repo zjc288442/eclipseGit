4b92b2d890680c305970b5d90fb900a0e99aba50
zjc288442 <56714947+zjc288442@users.noreply.github.com>
2020-02-23 22:07:09 +0800

    6666

--- src/main/java/com/credit/app/testGitBranch1.java
+++ src/main/java/com/credit/app/testGitBranch1.java
@@ -2,9 +2,10 @@ package com.credit.app;
 
 public class testGitBranch1 {
   public static void main(String[] args) {
-	System.out.println("22222222");
+    System.out.println("22222222");
     System.out.println("33333333");
     System.out.println("444444444");
     System.out.println("55555555");
+    System.out.println("66666666");	  
 }
 }
