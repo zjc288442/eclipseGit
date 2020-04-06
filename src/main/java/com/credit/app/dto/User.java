3bd49dfaa8d0c4f7453f4e4ded223c7e2ccfed71
zjc288442 <zjc_288442@126.com>
2020-04-06 13:18:00 +0800

    update for git server and add new change by zjc

---
+++ src/main/java/com/credit/app/dto/User.java
@@ -0,0 +1,34 @@
+package com.credit.app.dto;
+
+/**
+ * Created by Administrator on 2020/4/6.
+ */
+public class User {
+    String userId;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    String userName;
+}
