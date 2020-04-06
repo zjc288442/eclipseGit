4fc4b3d34c85a798c6040dd49f797a394ed02e1c
zjc288442 <zjc_288442@126.com>
2020-04-06 12:48:08 +0800

    update for git server and add new change by zjc

--- src/main/java/com/credit/app/TestGit.java
+++ src/main/java/com/credit/app/TestGit.java
@@ -3,7 +3,8 @@ package com.credit.app;
 public class TestGit {
   public static void main(String[] args) {
 	System.out.println("111111111111");
-        System.out.println("new 000000000000");
+    System.out.println("new 000000000000");
 	System.out.println("new 22222");
+	System.out.println("new 33333");
 }
 }
