d9539f28f9abb8339e970c8806649c46d08e17d5
zjc288442 <56714947+zjc288442@users.noreply.github.com>
2020-04-06 12:40:33 +0800

    Update TestGit.java

--- src/main/java/com/credit/app/TestGit.java
+++ src/main/java/com/credit/app/TestGit.java
@@ -3,7 +3,7 @@ package com.credit.app;
 public class TestGit {
   public static void main(String[] args) {
 	System.out.println("111111111111");
-
+        System.out.println("new 000000000000");
 	System.out.println("new 22222");
 }
 }
